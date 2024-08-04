5d7e35c39de1c495191e600fa53e261b88484c2e
Michał Olkuski <molkuski@MacBook-Pro-molkuski.local>
2024-08-05 01:08:11 +0200

    Nowa struktura plików

---
+++ frontend/.svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/kategorie/+page.svelte";
---
+++ frontend/.svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/produkty/+page.svelte";
